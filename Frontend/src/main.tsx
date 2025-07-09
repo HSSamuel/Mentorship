f0eacac0183fa11b3b9a458ccb459bf19510f1b0
Hunsa Semako S. <smkmayomisamuel@gmail.com>
2025-07-09 17:05:57 +0100

    Update Frontend and Backend

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,23 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { AuthProvider, useAuth } from "./contexts/AuthContext"; // Import useAuth
+import { AuthProvider } from "./contexts/AuthContext";
 import "./index.css";
-
-// This new component will act as a bridge between the AuthContext and the App component.
-const AppWithAuth = () => {
-  // We will add `appKey` to our AuthContext to trigger remounts.
-  const { appKey } = useAuth();
-  return <App key={appKey} />;
-};
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter
-      future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
-    >
+    <BrowserRouter>
       <AuthProvider>
-        <AppWithAuth />
+        <App />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
